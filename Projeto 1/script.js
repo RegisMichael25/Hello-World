9510c5a42c4e2b255f74d6d0530bed6686de04ef
Regis Michael <regis.michael101@gmail.com>
2024-02-13 19:15:05 -0300

    alterar nome para index

--- Projeto 1/script.js
+++ Projeto 1/script.js
@@ -5,7 +5,7 @@ sr.reveal('#titulo-imagens-animadas', {duration: 2000 });
 sr.reveal('#titulo-imagem-centro', {duration: 2000});
 
 sr.reveal('#imagem-centro', {
-    duration: 3000,
+    duration: 4000,
     rotate: {x: 0, y: 0, z: 1}
 })
 sr.reveal('#paragrafo-foto-centro',{
