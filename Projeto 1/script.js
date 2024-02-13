2ae8b2ce797f44645176169c2493010decddd988
Regis Michael <regis.michael101@gmail.com>
2024-02-13 18:29:01 -0300

    Ultilizando ScrollReveal
    Modifiquei as imagens no photoshop, e adicionei animações ao site com ScrollReveal.

---
+++ Projeto 1/script.js
@@ -0,0 +1,17 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('#titulo-imagens-animadas', {duration: 2000 });
+
+sr.reveal('#titulo-imagem-centro', {duration: 2000});
+
+sr.reveal('#imagem-centro', {
+    duration: 3000,
+    rotate: {x: 0, y: 0, z: 1}
+})
+sr.reveal('#paragrafo-foto-centro',{
+    duration: 3000
+})
+
+sr.reveal('#itens-animados', {
+    duration: 3000
+})
