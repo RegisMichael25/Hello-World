1eca8efc6f23cf5aabfd3d45d15052919a11b0cc
Regis Michael <regis.michael101@gmail.com>
2024-02-14 10:40:45 -0300

    Sistema Finalizado
    Sistema até então finalizado, aberto a melhoria de código.

--- Projeto 0/script.js
+++ Projeto 0/script.js
@@ -7,10 +7,10 @@ let girar = document.getElementById('girar');
 let ganhouPerdeu = document.getElementById('ganhou-perdeu');
 
 const btn1 = document.querySelector('button#btn-1');
-const btn2 = document.getElementById('#btn-2');
-const btn3 = document.getElementById('#btn-3');
-const btn4 = document.getElementById('#btn-4');
-const btn5 = document.getElementById('#btn-5');
+const btn2 = document.getElementById('btn-2');
+const btn3 = document.getElementById('btn-3');
+const btn4 = document.getElementById('btn-4');
+const btn5 = document.getElementById('btn-5');
 
 let numeroEscolhido = null;
 let moneyTemp = 0;
@@ -52,18 +52,66 @@ function subtrairDinheiro() {
 
 function botaoEscolhido1() {
     console.log(btn1.value)
+    numeroEscolhido = btn1.value;
+    console.log(numeroEscolhido);
 }
-function botaoEscolhido1() {
-    console.log(btn1.value)
+function botaoEscolhido2() {
+    console.log(btn2.value)
+    numeroEscolhido = btn2.value;
+    console.log(numeroEscolhido);
 }
-function botaoEscolhido1() {
-    console.log(btn1.value)
+function botaoEscolhido3() {
+    console.log(btn3.value)
+    numeroEscolhido = btn3.value;
+    console.log(numeroEscolhido);
 }
-function botaoEscolhido1() {
-    console.log(btn1.value)
+function botaoEscolhido4() {
+    console.log(btn4.value)
+    numeroEscolhido = btn4.value;
+    console.log(numeroEscolhido);
 }
-function botaoEscolhido1() {
-    console.log(btn1.value)
+function botaoEscolhido5() {
+    console.log(btn5.value)
+    numeroEscolhido = btn5.value;
+    console.log(numeroEscolhido);
 }
 
 
+function inicioAposta() {
+    if (moneyTemp == 0 && moneyUsuario !== 0){
+        alert('Você não fez aposta, automaticamente acrescentamos R$ 10.');
+        acrescenta10();
+        return;
+    }
+    
+    if(numeroEscolhido == null){
+        alert('Escolha um número.')
+        return;
+    }
+    
+    if(moneyUsuario == 0){
+        alert('Não é permitido apostar tudo, impossivel continuar.');
+        return;
+    }
+
+    numeroAleatorio = Math.round(Math.random() * (5 - 1)  + 1);
+    console.log(numeroAleatorio);
+    console.log(moneyTemp);
+    console.log(moneyUsuario);
+
+    if(numeroEscolhido == numeroAleatorio){
+        ganhouPerdeu.innerHTML = 'Parabéns, você ganhou.';
+        moneyUsuario = moneyUsuario + moneyTemp * 2
+        money.innerText = `R$ ${moneyUsuario}`;
+        moneyTemp = 0;
+        valor.innerHTML = `${moneyTemp}`;
+        numeroEscolhido = null;
+        return;
+    } else {
+        ganhouPerdeu.innerHTML = 'Perdeu';
+        moneyTemp = 0;
+        valor.innerHTML = `${moneyTemp}`;
+        numeroEscolhido = null;
+        return;
+    }
+}
