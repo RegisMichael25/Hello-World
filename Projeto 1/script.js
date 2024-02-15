ce8b292d2c6b5d1ca6b6408c443e3b8a26130828
Regis Michael <regis.michael101@gmail.com>
2024-02-15 19:08:01 -0300

    Incrementando responsividade
    Responsividade nos projetos.

--- Projeto 1/script.js
+++ Projeto 1/script.js
@@ -5,8 +5,7 @@ sr.reveal('#titulo-imagens-animadas', {duration: 2000 });
 sr.reveal('#titulo-imagem-centro', {duration: 2000});
 
 sr.reveal('#imagem-centro', {
-    duration: 4000,
-    rotate: {x: 0, y: 0, z: 1}
+    duration: 3500,
 })
 sr.reveal('#paragrafo-foto-centro',{
     duration: 3000
